aee0857227282ccbb84dce8d18d825aee69454e9
Max Zavaleta Villar <Max.Zavaleta@oechsle.pe>
2020-04-26 14:48:57 -0500

    Created

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+
+const app=express()
+
+app.use(express.json())
+
+//Routes
+
+app.get('/',function(req, res){
+
+    //res.json({"name":"Viajera","Interprete":"Frankie Ruiz"});
+
+    const song ={
+        "name":"Viajera",
+        "singer":[
+            "Frankie Ruiz",
+            "Fania All Star"
+        ],
+        "year":1963,
+        "author":"Carlos Farfan"
+    }
+    res.json(song)
+})
+
+app.listen(3000, function(){
+    console.log('Server listen in port ',3000)
+})
